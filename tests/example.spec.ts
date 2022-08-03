cd917c872d4e7fa700556602c65bf790b50065bc
Alberto Aragon Alvarez <ronin8503@gmail.com>
2022-08-03 11:12:25 +0100

    using screenshots

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -15,48 +15,55 @@ test('Click on Elements', async ({ page }) => {
   await expect(errorMessage).toContainText('Login and/or password are wrong')
 })
 
-// test('Selectors', async ({ page }) => {
-//   // text
-//   await page.click('text=some text')
+test.skip('Selectors', async ({ page }) => {
+  // text
+  await page.click('text=some text')
 
-//   // CSS Selectors
-//   await page.click('button')
-//   await page.click('#id')
-//   await page.click('.class')
+  // CSS Selectors
+  await page.click('button')
+  await page.click('#id')
+  await page.click('.class')
 
-//   // Only visible CSS Selectors
-//   await page.click('.submit-button:visible')
+  // Only visible CSS Selectors
+  await page.click('.submit-button:visible')
 
-//   // Combinations
-//   await page.click('#id .class')
+  // Combinations
+  await page.click('#id .class')
 
-//   // XPath
-//   await page.click('//*[@id="id"]')
-// })
-
-test('Working with Inputs', async ({ page }) => {
-  await page.goto('http://zero.webappsecurity.com/index.html')
-  await page.click('#signin_button')
-
-  await page.type('#user_login', 'some username')
-  await page.type('#user_password', 'some password')
-  await page.click('text=Sign in')
-
-  const errorMessage = await page.locator('.alert-error')
-  await expect(errorMessage).toContainText('Login and/or password are wrong')
+  // XPath
+  await page.click('//*[@id="id"]')
 })
 
-test('Assertions', async ({ page }) => {
+test.describe('My first test suite', () => {
+  test('Working with Inputs', async ({ page }) => {
+    await page.goto('http://zero.webappsecurity.com/index.html')
+    await page.click('#signin_button')
+
+    await page.type('#user_login', 'some username')
+    await page.type('#user_password', 'some password')
+    await page.click('text=Sign in')
+
+    const errorMessage = await page.locator('.alert-error')
+    await expect(errorMessage).toContainText('Login and/or password are wrong')
+  })
+
+  test('Assertions @myTag', async ({ page }) => {
+    await page.goto('https://example.com')
+
+    await expect(page).toHaveURL('https://example.com')
+    await expect(page).toHaveTitle('Example Domain')
+
+    const element = await page.locator('h1')
+    await expect(element).toBeVisible()
+    await expect(element).toHaveText('Example Domain')
+    await expect(element).toHaveCount(1)
+
+    const nonExistingElement = await page.locator('h5')
+    await expect(nonExistingElement).not.toBeVisible()
+  })
+})
+
+test.only('Screenshots', async ({ page }) => {
   await page.goto('https://example.com')
-
-  await expect(page).toHaveURL('https://example.com')
-  await expect(page).toHaveTitle('Example Domain')
-
-  const element = await page.locator('h1')
-  await expect(element).toBeVisible()
-  await expect(element).toHaveText('Example Domain')
-  await expect(element).toHaveCount(1)
-
-  const nonExistingElement = await page.locator('h5')
-  await expect(nonExistingElement).not.toBeVisible()
+  await page.screenshot({ path: 'example.png' })
 })
